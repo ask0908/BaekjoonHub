e4d904836c91b0759a37f16b8b543bfe9d53b19f
ask0908 <59863631+ask0908@users.noreply.github.com>
2022-05-08 01:52:26 +0900

    [Bronze IV] Title: 오븐 시계, Time: 232 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
 
-public class Main {
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int h = s.nextInt();
+        int m = s.nextInt();
+        int n = s.nextInt();
+        s.close();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int h = sc.nextInt();
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		
-		
-		// 모든 시간을 분으로 바꾼다!
-		int a = h*60 + m + n;
-		h = a/60;
-		m = a%60;
-		
-		//h가 24를 넘는다면?
-		if (h>=24) {
-			h= h-24;
-		}
-		
-		System.out.println(h + " "+ m);
+        int a = h * 60 + m + n;
+        h = a / 60;
+        m = a % 60;
 
-	}
+        if (h >= 24) {
+            h -= 24;
+        }
+        System.out.println(h + " " + m);
+    }
 }
