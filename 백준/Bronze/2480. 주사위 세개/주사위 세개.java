df06bd8592049e5133dd4b6af0b527ee5968a61a
ask0908 <59863631+ask0908@users.noreply.github.com>
2022-05-08 13:03:26 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -13,18 +13,21 @@ class Main {
         // 같은 눈이 3개인 경우
         if (firstNumber == secondNumber && secondNumber == thirdNumber) {
             System.out.println(10000 + (firstNumber * 1000));
-        } else if (firstNumber == secondNumber || firstNumber == thirdNumber) {
+        } else if (firstNumber == secondNumber || firstNumber == thirdNumber) { // else if : 같은 눈이 2개인 경우
             System.out.println(1000 + (firstNumber * 100));
         } else if (secondNumber == thirdNumber) {
             System.out.println(1000 + (secondNumber * 100));
         } else {
+            // 다 다른 눈일 경우
             Map<String, Integer> map = new HashMap<>();
             map.put("1", firstNumber);
             map.put("2", secondNumber);
             map.put("3", thirdNumber);
 
+            // value들을 가져와서 정렬
             Object[] mapKey = map.values().toArray();
             Arrays.sort(mapKey);
+            // 3칸 크기기 때문에 2번 index에 가장 큰 숫자가 들어있다
             int a = (int) mapKey[2];
             System.out.println(a * 100);
         }
